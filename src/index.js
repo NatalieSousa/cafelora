904a78f57b7f99c53e03452bb490a5e6ff15b5ec
Natalie Ferreira <natalie.fsousa@gmail.com>
2020-11-01 15:40:45 +0100

    První objednávka

--- src/index.js
+++ src/index.js
@@ -14,4 +14,22 @@ menuSchovany.addEventListener("click", () => {
   document.querySelector("nav").classList.toggle("nav-closed") 
 });
 
+const order = document.querySelector(".order-btn");
+const cup = document.querySelector(".drink__cup");
+let ordered = false;
+order.addEventListener("click", () => {
+  cup.classList.toggle("drink__cup--selected");
+  ordered = !ordered
+  if (ordered === true) {
+    order.textContent = "Objednat"
+  } else {
+    order.textContent = "Zrušit"
+  };
+});
+
+
+
+
+
+
 
