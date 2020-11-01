7fb322f10df353ffbed4446ff4872eed547b86ef
Natalie Ferreira <natalie.fsousa@gmail.com>
2020-11-01 14:45:25 +0100

    menu se zabalí

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const elm = document.getElementById("nav-btn");
+elm.addEventListener("click", () => {
+document.querySelector("nav").classList.toggle("nav-closed");
+}
+);
+
+const menuSchovany = document.querySelector(".list");
+menuSchovany.addEventListener("click", () => {
+  document.querySelector("nav").classList.toggle("nav-closed") 
+});
+
+
